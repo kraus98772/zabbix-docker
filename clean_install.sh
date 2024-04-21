8b7ae4f27d9451bdaf108ccfceb0ec96c387df82
kraus98772 <damian98772@gmail.com>
2024-04-21 19:13:39 +0200

    remove redundant compose files

---
+++ clean_install.sh
@@ -0,0 +1,3 @@
+docker container rm zabbix-docker-zabbix-server-1 zabbix-docker-mysql-server-1 zabbix-docker-db-data-mysql-1 zabbix-docker-zabbix-web-nginx-mysql-1
+docker volume rm zabbix-docker_snmptraps
+rm -rf zbx_env
