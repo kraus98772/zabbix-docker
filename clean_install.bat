34dd6520e166776c26a5fd4b59109ab51903b0b7
kraus98772 <damian98772@gmail.com>
2024-04-22 08:42:30 +0200

    cleanup

--- clean_install.bat
+++ clean_install.bat
@@ -1,4 +1,4 @@
 docker container rm zabbix-docker-zabbix-server-1 zabbix-docker-mysql-server-1 zabbix-docker-db-data-mysql-1 zabbix-docker-zabbix-web-nginx-mysql-1 zabbix-docker-zabbix-web-service-1 zabbix-docker-zabbix-snmptraps-1
 docker container rm zabbix-docker-zabbix-agent2-1
 docker volume rm zabbix-docker_snmptraps
-rm -rf zbx_env
+rmdir /s /q zbx_env
